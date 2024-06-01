591def41bc895d1427928b0bb135755fb82b5a76
GautamSoni <114256822+Gautamsonii@users.noreply.github.com>
2024-06-02 02:54:27 +0530

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-# Define your MongoDB URI here
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://vkdesk:j5ppkVBRYWtDX4KW@cluster0.jjjagoc.mongodb.net/scraperdb?retryWrites=true&w=majority&appName=Cluster0")
